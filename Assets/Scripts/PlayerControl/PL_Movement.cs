c1202e862eb8ae5bf3e885bee5aad00398e9da18
harry2love <hs405092@gmail.com>
2021-12-03 01:24:54 +0100

    Movementscript veranderd
    Het oude systeem werkte met translate, wat ervoor zorgde dat de player door muren kan gaan met weinig tot geen weerstand

--- Assets/Scripts/PlayerControl/PL_Movement.cs
+++ Assets/Scripts/PlayerControl/PL_Movement.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class PL_Movement : MonoBehaviour
 {
-    private float forwardSpeed = 10;
-    private float upwardForce = 2000000;
-    private float backForce = 5;
+    private float forwardSpeed = 10000;
+    private float upwardForce = 2000;
+    private float backForce = 10000;
     private float dashSpeed = 10000;
 
     private bool isGrounded = false;
     private bool dashActive = false;
 
+    public Vector3 direction1;
+    public Vector3 direction2;
+    public Vector3 direction3;
+    public Vector3 direction4;
+
     private Rigidbody rb;
 
     private enum keyboardLayout { AZERTY, QWERTY};
-    private keyboardLayout layout = keyboardLayout.AZERTY;
+    [SerializeField] private keyboardLayout layout = keyboardLayout.AZERTY;
 
     void Start()
     {
@@ -25,59 +30,93 @@ public class PL_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(layout == keyboardLayout.AZERTY)
+        switch (layout)
         {
-            if (Input.GetKey(KeyCode.Z))
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(Vector3.back * Time.deltaTime * backForce);
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                rb.AddForce(Vector3.up * Time.deltaTime * upwardForce, ForceMode.Impulse);
-                isGrounded = false;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                StartCoroutine(Dash());
-            }
+            case keyboardLayout.AZERTY:
+
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    direction1 = transform.TransformDirection(Vector3.forward * Time.deltaTime * forwardSpeed);
+                }
+                else if (!Input.GetKey(KeyCode.Z))
+                {
+                    direction1 = new Vector3(0, 0, 0);
+                }
+
+                if (Input.GetKey(KeyCode.S))
+                {
+                    direction2 = transform.TransformDirection(Vector3.back * Time.deltaTime * backForce);
+                }
+                else if (!Input.GetKey(KeyCode.S))
+                {
+                    direction2 = new Vector3(0, 0, 0);
+                }
+
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    direction3 = transform.TransformDirection(Vector3.left * Time.deltaTime * forwardSpeed);
+                }
+                else if (!Input.GetKey(KeyCode.Q))
+                {
+                    direction3 = new Vector3(0, 0, 0);
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    direction4 = transform.TransformDirection(Vector3.right * Time.deltaTime * forwardSpeed);
+                }
+                else if (!Input.GetKey(KeyCode.D))
+                {
+                    direction4 = new Vector3(0, 0, 0);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+                {
+                    rb.AddForce(Vector3.up * Time.deltaTime * upwardForce, ForceMode.Impulse);
+                    isGrounded = false;
+                }
+                Vector3 fulldirection = direction1 + direction2 + direction3 + direction4;
+                rb.velocity = new Vector3(fulldirection.x, rb.velocity.y, fulldirection.z);
+
+                break;
+
+            case keyboardLayout.QWERTY:
+
+                if (layout == keyboardLayout.QWERTY)
+                {
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+                    }
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        transform.Translate(Vector3.back * Time.deltaTime * backForce);
+                    }
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        transform.Translate(Vector3.left * Time.deltaTime * forwardSpeed);
+                    }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        transform.Translate(Vector3.right * Time.deltaTime * forwardSpeed);
+                    }
+                    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+                    {
+
+                        rb.AddForce(Vector3.up * Time.deltaTime * upwardForce, ForceMode.Impulse);
+                        isGrounded = false;
+                    }
+                }
+
+                break;
         }
-        else if(layout == keyboardLayout.QWERTY)
+
+         
+        if (!Input.anyKey)
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(Vector3.back * Time.deltaTime * backForce);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * forwardSpeed);
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                
-                rb.AddForce(Vector3.up * Time.deltaTime * upwardForce, ForceMode.Impulse);
-                isGrounded = false;
-            }
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
+        
 
         if (dashActive)
         {
